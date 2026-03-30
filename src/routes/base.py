ece3e836bbc9b743ef60c3564f3e410bde0e0fef
Emran Alhaddad <emran.alhaddad.dev@gmail.com>
2026-03-30 23:41:14 +0300

    refactor: remove dotenv loading from main.py and update base route to use pydantic settings

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,14 +1,13 @@
-from fastapi import FastAPI, APIRouter
-import os
+from fastapi import FastAPI, APIRouter, Depends
+from helpers.config import get_settings, Settings
 base_router = APIRouter(
     prefix="/api/v1",
-    tags=["api_v1"]
+    tags=["api_v1"] 
 )
 
 @base_router.get("/")
-async def read_root():
-    
+async def read_root(settings: Settings = Depends(get_settings)):
     return {
-        "app_name": os.getenv("APP_NAME"), 
-        "app_version": os.getenv("APP_VERSION")
+        "app_name": settings.APP_NAME, 
+        "app_version": settings.APP_VERSION
     }

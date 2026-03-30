0a09f71aa6662a03a0be6af1e1e53037922c9f3a
Emran Alhaddad <emran.alhaddad.dev@gmail.com>
2026-03-30 23:48:30 +0300

    feat: update .env.example and config.py to include file upload settings

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -4,6 +4,8 @@ class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
     OPENAI_API_KEY: str
+    FILE_ALLOWED_MIME_TYPES: list[str]
+    FILE_MAX_SIZE: int
 
     class Config:
         env_file = ".env"
